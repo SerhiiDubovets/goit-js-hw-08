86bf714cb78093e7abe5d46efadcc248324cedf5
SerhiiDubovets <Sergio.d@bigmir.net>
2022-05-08 21:51:19 +0300

    задание 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(KEY, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem(KEY))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const KEY_INPUT = 'feedback-form-state';
+
+populateTextarea();
+
+const formDate = {};
+
+input.addEventListener('submit', onFormSabmit);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('input', e => {
+  formDate[e.target.name] = e.target.value;
+  console.dir(formDate);
+});
+
+function onFormSabmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY_INPUT);
+}
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+  localStorage.setItem(KEY_INPUT, JSON.stringify(formDate));
+  console.log(message);
+}
+
+function populateTextarea() {
+  const saveMessage = localStorage.getItem(KEY_INPUT);
+  const parsKey = JSON.parse(saveMessage);
+  if (saveMessage) {
+    textarea.value = parsKey.message || '';
+    input.value = parsKey.email || '';
+  }
+}
